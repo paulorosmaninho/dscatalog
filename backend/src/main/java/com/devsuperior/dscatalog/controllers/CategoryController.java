835b5bdcad7e5798f71846db0fb75043dab402e3
Paulo Rosmaninho <paulo.rosmaninho@gmail.com>
2022-01-26 17:14:55 -0300

    CategoryController

---
+++ backend/src/main/java/com/devsuperior/dscatalog/controllers/CategoryController.java
@@ -0,0 +1,31 @@
+package com.devsuperior.dscatalog.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dscatalog.entities.Category;
+
+@RestController
+@RequestMapping(value = "/categories")
+//@RequestMapping(value) mapeia a rota do recurso. Normalmente no plural.
+public class CategoryController {
+	
+	
+	@GetMapping
+	public ResponseEntity<List<Category>> findAll(){
+		
+		List<Category> list = new ArrayList<>();
+		list.add(new Category(1L, "Books"));
+		list.add(new Category(2L, "Electronics"));
+		
+		return ResponseEntity.ok().body(list);
+	}
+	
+	
+
+}
