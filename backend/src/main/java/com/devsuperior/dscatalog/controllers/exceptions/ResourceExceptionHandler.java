47dca620d15e783b6fdbf048a63794f956865f76
Paulo Rosmaninho <paulo.rosmaninho@gmail.com>
2022-02-10 16:17:32 -0300

    Basic Validation Message

--- backend/src/main/java/com/devsuperior/dscatalog/controllers/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/devsuperior/dscatalog/controllers/exceptions/ResourceExceptionHandler.java
@@ -6,43 +6,58 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 
-
 @ControllerAdvice
 public class ResourceExceptionHandler {
-	
+
 	@ExceptionHandler(ResourceNotFoundException.class)
-	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
 		String error = "Recurso não encontrado.";
 		HttpStatus status = HttpStatus.NOT_FOUND;
 		StandardError stdError = new StandardError();
-		
+
 		stdError.setTimestampUTC(Instant.now());
 		stdError.setStatus(status.value());
 		stdError.setError(error);
 		stdError.setMessage(e.getMessage());
 		stdError.setPath(request.getRequestURI());
-		
+
 		return ResponseEntity.status(status).body(stdError);
 	}
 
 	@ExceptionHandler(DatabaseException.class)
-	public ResponseEntity<StandardError> resourceNotFound(DatabaseException e, HttpServletRequest request){
+	public ResponseEntity<StandardError> databaseError(DatabaseException e, HttpServletRequest request) {
 		String error = "Erro de violação de integridade.";
 		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError stdError = new StandardError();
-		
+
 		stdError.setTimestampUTC(Instant.now());
 		stdError.setStatus(status.value());
 		stdError.setError(error);
 		stdError.setMessage(e.getMessage());
 		stdError.setPath(request.getRequestURI());
-		
+
+		return ResponseEntity.status(status).body(stdError);
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+		String error = "Erro de validação.";
+		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+		StandardError stdError = new StandardError();
+
+		stdError.setTimestampUTC(Instant.now());
+		stdError.setStatus(status.value());
+		stdError.setError(error);
+		stdError.setMessage(e.getMessage());
+		stdError.setPath(request.getRequestURI());
+
 		return ResponseEntity.status(status).body(stdError);
 	}
 }
