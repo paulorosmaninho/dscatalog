0819169b7f9dae52a61b7382e4cd49e7e252e29a
Paulo Rosmaninho <paulo.rosmaninho@gmail.com>
2022-02-11 20:58:26 -0300

    Implements Spring Security

--- backend/src/main/java/com/devsuperior/dscatalog/config/WebSecurityConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/WebSecurityConfig.java
@@ -1,7 +1,9 @@
 package com.devsuperior.dscatalog.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -44,5 +46,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
 
 	}
+
+	@Override
+	@Bean /*Incluída a anotação @Bean*/
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
+	
+	
 
 }
