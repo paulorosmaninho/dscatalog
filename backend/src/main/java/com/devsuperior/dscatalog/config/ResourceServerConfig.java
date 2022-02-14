4a23812082ddb3a9affdbdea898667f51e52fdcf
Paulo Rosmaninho <paulo.rosmaninho@gmail.com>
2022-02-14 18:25:46 -0300

    H2 Security configuration

--- backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
@@ -1,7 +1,10 @@
 package com.devsuperior.dscatalog.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -13,15 +16,20 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
-	private static final String[] PUBLIC = { "/oauth/token" };
+	// Injetar o Bean JwtTokenStore
+	@Autowired
+	private JwtTokenStore jwtTokenStore;
+	
+	//Injetar a classe Environment para liberar o h2-console
+	@Autowired
+	private Environment env;
+	
+	private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" };
 
 	private static final String[] OPERATOR_OR_ADMIN = { "/products/**", "/categories/**" };
 
 	private static final String[] ADMIN = { "/users/**" };
 
-	// Injeta o Bean JwtTokenStore
-	@Autowired
-	private JwtTokenStore jwtTokenStore;
 
 	// Esse método é responsável por decodificar o token e analisar
 	// se as informações da origem, secret, expiration, etc. são válidas.
@@ -40,6 +48,12 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 
+		// Obtém os profiles ativos para liberar o h2-console
+		// Se contém "test" desabilita os frames para o h2-console executar
+		if(Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
+		
 		http.authorizeRequests()
 			.antMatchers(PUBLIC).permitAll()
 			.antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll()
